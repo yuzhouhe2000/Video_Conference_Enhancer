b523b92bac98a473aaa7830c29f6a7e991062a1f
MattBaseheart <basehear@usc.edu>
2021-05-08 21:23:39 -0700

    update EQ code

--- Processor/EQ/spectrogram.py
+++ Processor/EQ/spectrogram.py
@@ -1,12 +1,19 @@
 import matplotlib.pyplot as plt
+#import matplotlib.axes.Axes as ax
 import numpy as np
 from scipy import signal
 from scipy.io import wavfile
+from pydub import AudioSegment
 
-sample_rate, samples = wavfile.read('bp_filter.wav')
+#ASSUME THERE IS ALREADY A FILE CALLED TEST_MONO
+#convert = AudioSegment.from_wav('test.wav')
+#convert = convert.set_channels(1)
+#convert.export('test_mono.wav', format = 'wav')
+
+sample_rate, samples = wavfile.read('test_mono.wav')
 frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
 
-plt.pcolormesh(times, frequencies, np.log(spectrogram))
+plt.pcolormesh(times, frequencies, spectrogram)
 plt.ylabel('Frequency (Hz)')
 plt.xlabel('Time (s)')
 plt.show()
